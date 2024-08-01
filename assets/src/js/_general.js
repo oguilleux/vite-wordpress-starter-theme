9e57991b1d4f844bbfcc2a5e3e77289099c8f5d7
Olivier Guilleux <bonjour@olivier-guilleux.com>
2024-08-01 14:19:09 +0200

    cleaning

--- assets/src/js/_general.js
+++ assets/src/js/_general.js
@@ -8,24 +8,6 @@ class General {
 		// for tests purposes only
 		console.log(this.testVariable);
 	}
-
-	testing() {
-		// for tests purposes only
-		console.log('testing');
-
-		  console.log('testing');
-
-
-
-		// here is me commt
-
-		console.log('testing');
-	}
-
-	newFunction() {
-		// for tests purposes only
-		console.log('testing');
-	}
 }
 
 export default General;
